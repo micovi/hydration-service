a08d097a6a4da4cb65afc41404d2c6fa73709046
micovi <micleaovidiul@gmail.com>
2025-10-06 15:52:58 +0300

    fix base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/hydration-service/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
